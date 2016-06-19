9ec5400a11ece8422d9d1a5c00b010436e0c8514
Jordaanm <jordaanm@gmail.com>
2016-06-19 12:28:28 +1000

    Basic Redux App

--- src/client/index.js
+++ src/client/index.js
@@ -1,2 +1,22 @@
-let message = "Hello World";
-console.log(message);
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import Counter from '../common/components/Counter'
+import counterReducer from '../common/reducers'
+
+const store = createStore(counterReducer);
+const rootEl = document.getElementById('root');
+
+function render() {
+	ReactDOM.render(
+		<Counter
+			value={store.getState()}
+			onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+			onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+		/>,
+		rootEl  
+	);
+}
+
+render();
+store.subscribe(render);

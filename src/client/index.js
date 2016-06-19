7314d629508aa776877a005acf98dda0638897f5
Jordaanm <jordaanm@gmail.com>
2016-06-19 14:14:51 +1000

    LESS module loading via Webpack

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import Counter from '../common/components/Counter'
-import counterReducer from '../common/reducers'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import Counter from '../common/components/Counter';
+import counterReducer from '../common/reducers';
+import './../styles/app.less';
 
 const store = createStore(counterReducer);
 const rootEl = document.getElementById('root');
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,39 @@
 var path = require('path')
 var webpack = require('webpack')
+var ExtractTextPlugin = require("extract-text-webpack-plugin")
 
 module.exports = {
-  devtool: 'eval-source-map',
-  entry: [
-    './src/client/index.js'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/assets/js/'
-  },
-  plugins: [],
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loaders: [ 'babel' ],
-        exclude: /node_modules/,
-        include: __dirname
-      }
-    ]
-  }
+	devtool: 'source-map',
+	entry: [
+		'./src/client/index.js'
+	],
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'bundle.js',
+        chunkFilename: "[id].js",
+		publicPath: '/assets/'
+	},
+	plugins: [
+		new ExtractTextPlugin('styles.css', {allChunks: true})
+	],
+	module: {
+		preLoaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'source-map'
+			}
+		],
+		loaders: [
+			{
+				test: /\.less$/, include: path.join(__dirname, '/src/styles'), loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
+			},
+			{
+				test: /\.js$/,
+				loaders: [ 'babel' ],
+				exclude: /node_modules/,
+				include: __dirname
+			}
+		]
+	}
 }

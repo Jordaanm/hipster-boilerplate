d6232a1f36db325c72f4d08bfe0e6fc4e1cd570f
Jordaanm <jordaanm@gmail.com>
2016-06-19 11:32:10 +1000

    Babel ES6 transformation for server

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,2 @@
-// require('babel/register');
-// require('../index.js');
-
-var express = require("express");
-var app = express();
-
-app.get("/", function(req, res){
-	res.send("Hello World!");
-});
-
-app.listen(3000, function() {
-	console.log("Example app listening on port 3000");
-});
+require('babel-register');
+require('../index.js');
---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+let app = express();
+
+app.get("/", function(req, res){
+	res.send("Hello World!");
+});
+
+app.listen(3000, function() {
+	console.log("Example app listening on port 3000");
+});
